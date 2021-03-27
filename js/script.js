deac27a9a1ba83ebb2a1ca0281aa8b7ad8587b09
Elson Ramos <elson.0509@gmail.com>
2021-03-27 14:50:16 -0400

    fixing left space in input text

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ let inputTouched = {
 
 const inputEmail = document.getElementById("inputEmail")
 const inputPassword = document.getElementById("inputPassword")
+const inputWrapperEmail = document.getElementById("input-wrapper-email")
+const inputWrapperPassword = document.getElementById("input-wrapper-password")
 const warningEmail = document.getElementById("warningEmail")
 const warningPassword = document.getElementById("warningPassword")
 
